6f1632906ca3d7c1f76ac32d42533efa5b6fbb71
Brent Mckinney <148820886+brentrmck@users.noreply.github.com>
2025-03-26 12:29:15 -0400

    added db containers (i think)

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'dev-key'  # or use os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tracker.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'tracker.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
